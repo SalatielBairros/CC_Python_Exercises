bf2dda71dbc3223c5b734a14c9e094e4c2b95776
Salatiel Bairros <salatiel.costabairros@gmail.com>
2019-12-05 13:56:45 -0300

    Algoritmo do cubic root

--- cubic_root.py
+++ cubic_root.py
@@ -4,11 +4,21 @@ def cubic_root(n):
 
     if(n >= 0):
         if (n < 2):
-          return n
+          response = n
+        else:
+          while((response * response * response) <= n):
+              response += 1
+          response -= 1
 
-        cube = response * response * response
-        while(cube < n):
-            response += 1
-            cube = response * response * response
+        diff = n - (response * response * response)
+        isExact = "exact!" if diff == 0 else "not exact"
+        # Interpolation does not work at Python 2.
+        # diffMessage = f"with difference {diff}"
+        diffMessage = "" if diff == 0 else "with difference " + str(diff)
+        message = str(response) + ", " + isExact + " " + diffMessage
+        # print(f"{response}, {isExact} {diffMessage}")
+        print(message)
 
-        response -= 1
+    pass
+
+cubic_root(99)
